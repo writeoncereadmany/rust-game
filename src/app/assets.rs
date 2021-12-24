38429cac572115dea910785f5018a95acf65d71a
Tom Johnson <tarjohnson1979@gmail.com>
2021-12-24 12:56:46 +0000

    Start structuring assets in preparation for adding level assets

--- src/app/assets.rs
+++ src/app/assets.rs
@@ -12,11 +12,12 @@ pub struct Assets<'a> {
 impl <'a> Assets<'a> {
     pub fn new(texture_creator : &'a TextureCreator<WindowContext>) -> Result<Self, String> {
         let assets = find_folder::Search::ParentsThenKids(3,3).for_folder("assets").unwrap();
+        let graphics = assets.join("graphics");
 
-        let spritesheet = texture_creator.load_texture(assets.join("ball.png"))?;
-        let tilesheet = texture_creator.load_texture(assets.join("12x12tile.png"))?;
-        let numbersheet = texture_creator.load_texture(assets.join("numbers.png"))?;
-        let spritefont = texture_creator.load_texture(assets.join("spritefont.png"))?;
+        let spritesheet = texture_creator.load_texture(graphics.join("ball.png"))?;
+        let tilesheet = texture_creator.load_texture(graphics.join("12x12tile.png"))?;
+        let numbersheet = texture_creator.load_texture(graphics.join("numbers.png"))?;
+        let spritefont = texture_creator.load_texture(graphics.join("spritefont.png"))?;
 
         Ok(Assets {
             spritesheet,
