ed77b4f8aed6fb35a7ac655fd4d4c24984e16f5b
Tom Johnson <tarjohnson1979@gmail.com>
2022-05-01 13:59:19 +0100

    Add FutureEvent as an event type to be fired, to permit events to be scheduled through the event loop

--- src/timer_events.rs
+++ src/timer_events.rs
@@ -2,9 +2,23 @@ use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::time::{Instant, Duration};
 
+use component_derive::Event;
+
 use crate::game_loop::{ Event, EventTrait, Events };
 
-pub struct TimerEvent {
+#[derive(Event)]
+pub struct FutureEvent {
+    delay: Duration,
+    event: Event
+}
+
+impl FutureEvent {
+    fn in_ms<E: EventTrait>(ms: u64, event: E) -> Self {
+        FutureEvent { delay: Duration::from_millis(ms), event: Event::new(event) }
+    }
+}
+
+struct TimerEvent {
     fires_at: Instant,
     event: Event
 }
@@ -36,15 +50,15 @@ pub struct TimerEvents {
 }
 
 impl TimerEvents {
-    fn new(now: Instant) -> Self {
+    pub fn new(now: Instant) -> Self {
         TimerEvents { current_time: now, events: BinaryHeap::new() }
     }
 
-    fn schedule<E: EventTrait>(&mut self, dt: Duration, event: E) {
-        self.events.push(TimerEvent { fires_at: self.current_time + dt, event: Event::new(event) });
+    pub fn schedule(&mut self, event: FutureEvent) {
+        self.events.push(TimerEvent { fires_at: self.current_time + event.delay, event: event.event});
     }
 
-    fn elapse(&mut self, dt: Duration, events: &mut Events) {
+    pub fn elapse(&mut self, dt: Duration, events: &mut Events) {
         self.current_time += dt;
         while let Some(event) = self.pop() {
             events.fire_wrapped(event);
@@ -83,7 +97,7 @@ mod tests {
     #[test]
     fn returns_events_once_sufficient_time_has_passed() {
         let mut timer = TimerEvents::new(Instant::now());
-        timer.schedule(Duration::from_millis(300), TestEvent(1));
+        timer.schedule(FutureEvent::in_ms(300, TestEvent(1)));
 
         assert_elapsed_events(vec![], &mut timer, Duration::from_millis(200));
         assert_elapsed_events(vec![&TestEvent(1)], &mut timer, Duration::from_millis(200));
@@ -92,9 +106,10 @@ mod tests {
     #[test]
     fn orders_events_in_order_of_time_to_fire() {
         let mut timer = TimerEvents::new(Instant::now());
-        timer.schedule(Duration::from_millis(300), TestEvent(1));
-        timer.schedule(Duration::from_millis(200), TestEvent(2));
-        timer.schedule(Duration::from_millis(400), TestEvent(3));
+
+        timer.schedule(FutureEvent::in_ms(300, TestEvent(1)));
+        timer.schedule(FutureEvent::in_ms(200, TestEvent(2)));
+        timer.schedule(FutureEvent::in_ms(400, TestEvent(3)));
 
         assert_elapsed_events(
             vec![&TestEvent(2), &TestEvent(1), &TestEvent(3)], 
@@ -106,18 +121,17 @@ mod tests {
     fn orders_events_in_order_of_time_to_fire_if_scheduled_after_time_passes() {
         let mut timer = TimerEvents::new(Instant::now());
 
-        timer.schedule(Duration::from_millis(300), TestEvent(1));
-        timer.schedule(Duration::from_millis(200), TestEvent(2));
-        timer.schedule(Duration::from_millis(400), TestEvent(3));
+        timer.schedule(FutureEvent::in_ms(300, TestEvent(1)));
+        timer.schedule(FutureEvent::in_ms(200, TestEvent(2)));
+        timer.schedule(FutureEvent::in_ms(400, TestEvent(3)));
 
         assert_elapsed_events(
             vec![&TestEvent(2), &TestEvent(1)], 
             &mut timer, Duration::from_millis(350)
         );
 
-        timer.schedule(Duration::from_millis(25), TestEvent(4)); // should fire at 375
-        timer.schedule(Duration::from_millis(75), TestEvent(5)); // should fire at 425
-
+        timer.schedule(FutureEvent::in_ms(25, TestEvent(4)));
+        timer.schedule(FutureEvent::in_ms(75, TestEvent(5)));
 
         assert_elapsed_events(
             vec![&TestEvent(4), &TestEvent(3), &TestEvent(5)], 
