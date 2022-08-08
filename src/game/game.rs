07dca29a23b44123216740ab39d150f6dca13758
Tom Johnson <tarjohnson1979@gmail.com>
2022-08-08 15:26:05 +0100

    Make the coin sounds a little less... stubby?

--- src/game/game.rs
+++ src/game/game.rs
@@ -35,7 +35,7 @@ impl <'a> GameLoop<'a, Renderer<'a>> for Game<'a> {
             self.score += 10;
             events.fire(PlayTune(vec![
                 (Duration::from_millis(0), Note::Wave { pitch: B * 4.0, envelope: EnvSpec::vols(vec![(0.0, 0.25), (0.3, 0.0)]) }),
-                (Duration::from_millis(60), Note::Wave { pitch: E * 4.0, envelope: EnvSpec::vols(vec![(0.0, 0.25), (0.3, 0.0)]) })
+                (Duration::from_millis(60), Note::Wave { pitch: E * 4.0, envelope: EnvSpec::vols(vec![(0.0, 0.25), (0.5, 0.0)]) })
             ]));
         });
         event.apply(|TimeLimitExpired| {
