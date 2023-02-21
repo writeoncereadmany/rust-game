732e39724602bb54df9d914cb50e6a93f3d05a7d
Tom Johnson <tarjohnson1979@gmail.com>
2023-02-21 11:34:09 +0000

    Add a concept of bars to the tune builder

--- src/audio/tempo.rs
+++ src/audio/tempo.rs
@@ -17,28 +17,36 @@ impl Tempo {
             tempo: self,
             instrument,
             channel,
+            bar: 1,
             notes: Vec::new()
         }
     }
 
-    fn beat(&self, beat: f32, length: f32) -> (f32, f32) {
-        // beats count from 1, not 0
-        (self.beat_length * (beat - 1.0), self.beat_length * length)
+    fn beat(&self, beat: f32, bar: usize, length: f32) -> (f32, f32) {
+        // beats and bars count from 1, not 0
+        let total_beat = ((bar - 1) * self.beats) as f32 + (beat - 1.0);
+        (self.beat_length * total_beat, self.beat_length * length)
     }
 }
 
 struct TuneBuilder<'a> {
     tempo: &'a Tempo,
     instrument: &'a Instrument,
-    channel: usize, 
+    channel: usize,
+    bar: usize,
     notes: Vec<(Duration, Note)>
 }
 
 impl <'a> TuneBuilder<'a> {
 
-    fn play(mut self, pitch: f32, octave: i32, beat: f32, length: f32) -> Self {
-        let (start, len) = self.tempo.beat(beat, length);
+    fn play(mut self, beat: f32, length: f32, pitch: f32, octave: i32) -> Self {
+        let (start, len) = self.tempo.beat(beat, self.bar, length);
         self.notes.push((Duration::from_secs_f32(start), self.instrument.note(pitch, octave, len)));
+        self
+    }
+
+    fn bar(mut self, bar: usize) -> Self {
+        self.bar = bar;
         self
     }
 
@@ -59,8 +67,8 @@ mod tests {
     fn converts_beats_into_times() {
         let tune = Tempo::new(4, 60)
             .using(&BELL, 2)
-            .play(C, 3, 1.0, 1.0)
-            .play(D, 3, 2.0, 1.0)
+            .play(1.0, 1.0, C, 3)
+            .play(2.0, 1.0, D, 3)
             .build();
 
         assert_eq!(tune, PlayTune(2, vec![
@@ -73,13 +81,52 @@ mod tests {
     fn plays_tunes_faster_with_higher_bpm() {
         let tune = Tempo::new(4, 120)
             .using(&BELL, 2)
-            .play(C, 3, 1.0, 1.0)
-            .play(D, 3, 2.0, 1.0)
+            .play(1.0, 1.0, C, 3)
+            .play(2.0, 1.0, D, 3)
             .build();
 
         assert_eq!(tune, PlayTune(2, vec![
             (Duration::from_millis(0), BELL.note(C, 3, 1.0)),
             (Duration::from_millis(500), BELL.note(D, 3, 1.0))
+        ]));
+    }
+
+    #[test]
+    fn handles_bars() {
+        let tune = Tempo::new(4, 120)
+            .using(&BELL, 2)
+            .play(1.0, 1.0, C, 3)
+            .play(2.0, 1.0, D, 3)
+            .bar(2)
+            .play(1.0, 1.0, E, 3)
+            .play(2.0, 1.0, D, 3)
+            .build();
+
+        assert_eq!(tune, PlayTune(2, vec![
+            (Duration::from_millis(0), BELL.note(C, 3, 1.0)),
+            (Duration::from_millis(500), BELL.note(D, 3, 1.0)),
+            (Duration::from_millis(2000), BELL.note(E, 3, 1.0)),
+            (Duration::from_millis(2500), BELL.note(D, 3, 1.0)),
+        ]));
+    }
+
+
+    #[test]
+    fn handles_bars_with_different_time_signatures() {
+        let tune = Tempo::new(3, 120)
+            .using(&BELL, 2)
+            .play(1.0, 1.0, C, 3)
+            .play(2.0, 1.0, D, 3)
+            .bar(2)
+            .play(1.0, 1.0, E, 3)
+            .play(2.0, 1.0, D, 3)
+            .build();
+
+        assert_eq!(tune, PlayTune(2, vec![
+            (Duration::from_millis(0), BELL.note(C, 3, 1.0)),
+            (Duration::from_millis(500), BELL.note(D, 3, 1.0)),
+            (Duration::from_millis(1500), BELL.note(E, 3, 1.0)),
+            (Duration::from_millis(2000), BELL.note(D, 3, 1.0)),
         ]));
     }
 }
