69df8f8a8431ff4b4c341ed7bab9d61276e1a725
Tom Johnson <tarjohnson1979@gmail.com>
2024-09-05 10:58:46 +0100

    start using tiled library: so far, just import and load tilesheet textures

--- pandamonium/src/app/assets.rs
+++ pandamonium/src/app/assets.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
-
 use image::RgbImage;
+use std::collections::HashMap;
+use std::path::PathBuf;
 
 use sdl2::image::LoadTexture;
 use sdl2::render::{Texture, TextureCreator};
@@ -12,6 +12,7 @@ pub struct Assets<'a> {
     pub levels: Vec<RgbImage>,
     pub countdown: RgbImage,
     pub go: RgbImage,
+    pub sheets : HashMap<String, Texture<'a>>
 
 }
 
@@ -25,6 +26,17 @@ impl <'a> Assets<'a> {
         let spritefont = texture_creator.load_texture(graphics.join("spritefont.png"))?;
         let countdown = image::open(graphics.join("countdown.png")).unwrap().to_rgb8();
         let go = image::open(graphics.join("go.png")).unwrap().to_rgb8();
+
+        let mut sheets = HashMap::new();
+
+        let mut map_loader = tiled::Loader::new();
+        let map = map_loader.load_tmx_map(assets.join("maps").join("001.tmx")).map_err(|err| format!("{err:?}"))?;
+        for tileset in map.tilesets().iter() {
+            if let Some(image) = &tileset.image {
+                let texture = texture_creator.load_texture(&image.source)?;
+                sheets.insert(tileset.name.to_string(), texture);
+            }
+        }
 
 
         let mut levels: Vec<PathBuf> = assets.join("levels").read_dir()
@@ -43,7 +55,8 @@ impl <'a> Assets<'a> {
             spritefont,
             levels,
             countdown,
-            go
+            go,
+            sheets
         })
     }
 }
