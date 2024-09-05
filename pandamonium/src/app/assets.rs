62eaa3374b52afd49c684c1b8ada6f316a6617d6
Tom Johnson <tarjohnson1979@gmail.com>
2024-09-05 13:57:00 +0100

    load map into memory, albeit with some naive data structures. we're not using it yet.

--- pandamonium/src/app/assets.rs
+++ pandamonium/src/app/assets.rs
@@ -5,6 +5,7 @@ use std::path::PathBuf;
 use sdl2::image::LoadTexture;
 use sdl2::render::{Texture, TextureCreator};
 use sdl2::video::WindowContext;
+use tiled::TileId;
 
 pub struct Assets<'a> {
     pub spritesheet : Texture<'a>,
@@ -12,8 +13,9 @@ pub struct Assets<'a> {
     pub levels: Vec<RgbImage>,
     pub countdown: RgbImage,
     pub go: RgbImage,
-    pub sheets : HashMap<String, Texture<'a>>
-
+    pub sheets : HashMap<String, Texture<'a>>,
+    pub tiles : HashMap<(usize, TileId), (String, (u32, u32), Option<String>)>,
+    pub map : Vec<HashMap<(u32, u32), (usize, TileId)>>
 }
 
 impl <'a> Assets<'a> {
@@ -28,13 +30,37 @@ impl <'a> Assets<'a> {
         let go = image::open(graphics.join("go.png")).unwrap().to_rgb8();
 
         let mut sheets = HashMap::new();
+        let mut tiles = HashMap::new();
+        let mut map = Vec::new();
 
         let mut map_loader = tiled::Loader::new();
-        let map = map_loader.load_tmx_map(assets.join("maps").join("001.tmx")).map_err(|err| format!("{err:?}"))?;
-        for tileset in map.tilesets().iter() {
+        let tile_map = map_loader.load_tmx_map(assets.join("maps").join("001.tmx")).map_err(|err| format!("{err:?}"))?;
+        for (index, tileset) in tile_map.tilesets().iter().enumerate() {
+
+            let name = tileset.name.to_string();
+
+            let columns = tileset.columns;
+
+            for (tile_id, tile) in tileset.tiles() {
+                tiles.insert((index, tile_id), (name.clone(), (tile_id % columns, tile_id / columns), tile.user_type.clone()));
+            }
+
             if let Some(image) = &tileset.image {
-                let texture = texture_creator.load_texture(&image.source)?;
-                sheets.insert(tileset.name.to_string(), texture);
+                sheets.insert(name, texture_creator.load_texture(&image.source)?);
+            }
+        }
+
+        for layer in tile_map.layers() {
+            if let Some(tiles) = layer.as_tile_layer() {
+                let mut map_layer = HashMap::new();
+                for x in 0..tiles.width().unwrap() {
+                    for y in 0..tiles.height().unwrap() {
+                        if let Some(tile) = tiles.get_tile(x as i32, y as i32) {
+                            map_layer.insert((x, y), (tile.tileset_index(), tile.id()));
+                        }
+                    }
+                }
+                map.push(map_layer);
             }
         }
 
@@ -56,7 +82,9 @@ impl <'a> Assets<'a> {
             levels,
             countdown,
             go,
-            sheets
+            sheets,
+            tiles,
+            map
         })
     }
 }
